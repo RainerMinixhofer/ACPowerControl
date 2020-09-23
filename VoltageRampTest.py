f8b618acc910e5477db2de4d5db89cbb55abfb23
Rainer Minixhofer <rainer.minixhofer@dialogus.a>
2020-09-23 23:31:53 +0200

    Now VoltageRampTest.py is working. Solved EMC issues by redefining switching sequence to Coils,110parallel/serial and then mains. Switching off in reverse order. Calibrated Voltage Measuring by adjusting assumption on Rvbus.

--- VoltageRampTest.py
+++ VoltageRampTest.py
@@ -10,47 +10,89 @@ from math import sqrt
 from MCP23017 import MCP23017
 import INA260 #pylint: disable=E0401
 
-def switchsequence(portextender, powermeter, gpa, gpb, Vac):
+#Define voltage ladder one gets from the valid combination of the 3V, and 2x6V
+#secondary coils of the transformer for low current (1A) and the high current
+#(2A) option
+validV1A = []
+for i in range(10):
+    validV1A.append(1.5 * (i+1))
+for i in range(5):
+    validV1A.append(3.0 * (i+1) + validV1A[9])
+validV2A = []
+for i in range(6):
+    validV2A.append(1.5 * (i+1))
+for i in range(2):
+    validV2A.append(3.0 * (i+1) + validV2A[5])
+
+#Define the corresponding register settings for the required connections
+#for the above defined voltage ladder. The MSB byte is port A the LSB byte is port B
+reg1A = [0x0C06, 0x0C00, 0x1406, 0x1080, 0x2406, 0x1400, 0x9206, 0x2080, 0x6106, \
+         0x2400, 0x8900, 0x9200, 0x6200, 0x6100, 0xA100]
+dict1A = dict(zip(validV1A, reg1A))
+reg2A = [0x094E, 0x0948, 0x1156, 0x1218, 0x2166, 0x1150, 0x2228, 0x2160]
+dict2A = dict(zip(validV2A, reg2A))
+
+def voltage2registers(voltage, highcurrent=False):
+    """
+    Converts the specified ac voltage in the tuple <allowed Vac>, <register a> and
+    <register b> and returns the values
+    """
+    #Limit voltage to allowed values. Take closest allowed value from validVxA lists
+    voltage = min(validV2A if highcurrent else validV1A, key=lambda x: abs(x-voltage))
+    regs = dict2A[voltage] if highcurrent else dict1A[voltage]
+    return voltage, regs >> 8, regs & 0xFF
+
+def switchsequence(portextender, powermeter, Vac, highcurrent=False):
     """
 
     Parameters
     ----------
-    gpa : hexadecimal
-        value to write into gpioa register for relais setting of port gpa.
-    gpb : hexadecimal
-        value to write into gpiob register for relais setting of port gpb.
+    highcurrent : Boolean
+        If True the register settings for 2A configuration is returned, if
+        False then the register setting for 1A is returned
+    portextender, powermeter : object
+        Objects for the handling of the portextender and the powermeter respectively
     Vac : Integer
         Specifies AC Voltage given by the definition of gpa and gpb ports.
 
     Returns
     -------
-    None.
+    None
 
     """
 
+    #Get the required register settings for specified voltage Vac
+    Vac, gpa, gpb = voltage2registers(Vac, highcurrent=highcurrent)
     #Set relais positions for required voltage and leave mains pin off
     #Then switch on mains
     portextender.setregister("gpioa", gpa)
     portextender.setregister("gpiob", gpb)
     portextender.enable('Mains')
+    time.sleep(1.0)
     #Readout on voltmeter, wait 1s to get voltage settled and to enable measurement integration
-    time.sleep(2.0)
     #Since we have a single rectifier in the sensing circuit
     #The AC effective voltage measured is sqrt(2) times lower than the
     #effective voltage
-    print("Measuring....")
-    powermeter.alert = ['Conversion Ready']
-    assert powermeter.wait_for_alert_edge(timeout='Automatic'), \
+    effvoltage = 0
+    count = 0
+    while abs(effvoltage-Vac)>1.0 and (count<1):
+        print("Measuring....")
+        powermeter.alert = ['Conversion Ready']
+        assert powermeter.wait_for_alert_edge(timeout='Automatic'), \
         print("Timeout of conversion ready detection")
-    effvoltage = powermeter.voltage() * sqrt(2)
-    print("Voltage for ", Vac, "V", effvoltage)
+        effvoltage = powermeter.voltage() * sqrt(2)
+        print("Voltage setting: {:4.1f}V ({:5.2f}V measured)".format(Vac, effvoltage))
+        count += 1
     #Wait for voltage peak to minimize EMC when switching off mains
     powermeter.wait_for_voltage_peak()
     #Switch mains off
     portextender.disable('Mains')
-    time.sleep(0.5)
-    portextender.reset()
-    portextender.all_to_output()
+    time.sleep(1.0)
+    portextender.setregister("gpioa", 0x00)
+    portextender.setregister("gpiob", 0x00)
+    time.sleep(1.0)
+#    portextender.reset()
+#    portextender.all_to_output()
 
 #create chip driver with bank=0 mode on address 0x20
 mcp23017 = MCP23017(i2cbus=1, device=0x20, bank=0, resetpin=4)
@@ -72,58 +114,16 @@ assert mcp23017.getregister("iodirb") == 0x00, \
 #ina260 = INA260.INA260Controller(alertpin=13, avg=1024, vbusct=1100, meascont=True, \
 #                                 measi=False, measv=True, Rdiv1=220, alert = ['Conversion Ready'])
 ina260 = INA260.INA260Controller(alertpin=13, avg=1024, vbusct=1100, ishct=140, meascont=True, \
-                                 measi=False, measv=True, Rdiv1=220)
+                                 measi=False, measv=True, Rdiv1=220, Rvbus=340)
 
 print("Ramping Voltage from 1.5V up to 30V in 1A coil configuration")
 input("Press enter when ready....")
 
-#1.5V/1A
-switchsequence(mcp23017, ina260, 0x0C, 0x06, 1.5)
-#3.0V/1A
-switchsequence(mcp23017, ina260, 0x0C, 0x00, 3.0)
-#4.5V/1A
-switchsequence(mcp23017, ina260, 0x14, 0x06, 4.5)
-#6.0V/1A
-switchsequence(mcp23017, ina260, 0x10, 0x80, 6.0)
-#7.5V/1A
-switchsequence(mcp23017, ina260, 0x24, 0x06, 7.5)
-#9.0V/1A
-switchsequence(mcp23017, ina260, 0x14, 0x00, 9.0)
-#10.5V/1A
-switchsequence(mcp23017, ina260, 0x92, 0x06, 10.5)
-#12.0V/1A
-switchsequence(mcp23017, ina260, 0x20, 0x80, 12.0)
-#13.5V/1A
-switchsequence(mcp23017, ina260, 0x61, 0x06, 13.5)
-#15.0V/1A
-switchsequence(mcp23017, ina260, 0x24, 0x00, 15.0)
-#18.0V/1A
-switchsequence(mcp23017, ina260, 0x89, 0x00, 18.0)
-#21.0V/1A
-switchsequence(mcp23017, ina260, 0x92, 0x00, 21.0)
-#24.0V/1A
-switchsequence(mcp23017, ina260, 0x62, 0x00, 24.0)
-#27.0V/1A
-switchsequence(mcp23017, ina260, 0x61, 0x00, 27.0)
-#30.0V/1A
-switchsequence(mcp23017, ina260, 0xA1, 0x00, 30.0)
+for vac in validV1A:
+    switchsequence(mcp23017, ina260, vac, highcurrent=False)
 print("Ramping Voltage from 1.5V up to 15V in 2A coil configuration")
-#1.5V/2A
-switchsequence(mcp23017, ina260, 0x09, 0x4E, 1.5)
-#3.0V/2A
-switchsequence(mcp23017, ina260, 0x09, 0x48, 3.0)
-#4.5V/2A
-switchsequence(mcp23017, ina260, 0x11, 0x56, 4.5)
-#6.0V/2A
-switchsequence(mcp23017, ina260, 0x12, 0x18, 6.0)
-#7.5V/2A
-switchsequence(mcp23017, ina260, 0x21, 0x66, 7.5)
-#9.0V/2A
-switchsequence(mcp23017, ina260, 0x50, 0x51, 9.0)
-#12.0V/2A
-switchsequence(mcp23017, ina260, 0x22, 0x28, 12.0)
-#15.0V/2A
-switchsequence(mcp23017, ina260, 0x21, 0x60, 15.0)
+for vac in validV2A:
+    switchsequence(mcp23017, ina260, vac, highcurrent=True)
 
 del mcp23017
 del ina260
